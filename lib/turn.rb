39e940a6c03401b0b674750227a6b234c463a200
delightful-dispatch-9896 <maychen2580@hotmail.com>
2020-08-01 16:09:29 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -30,7 +30,7 @@ def place_taken?(board, index)
 end
 
 def turn(board)
-  puts "Please enter 1–9: "
+  puts "Please enter 1–9:"
   input = gets.strip
   index = input_to_index(input)
  if valid_move?(board, index)
