a81e34bd1ceecf5ce8c2e5d20300752d1b4bf155
delightful-dispatch-9896 <maychen2580@hotmail.com>
2020-08-01 15:38:10 +0000

    Automatically backed up by Learn

---
+++ lib/turn.rb
@@ -0,0 +1,38 @@
+def display_board(board)
+puts " #{board[0]} | #{board[1]} | #{board[2]} "
+puts "-----------"
+puts " #{board[3]} | #{board[4]} | #{board[5]} "
+puts "-----------"
+puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
+
+#change input to Integer
+def input_to_index(user_input)
+  user_input.to_i-1
+end
+
+ def place_taken?(board, index)
+  !(board[index].nil? || board[index] == " ")
+end
+
+def valid_move?(board, index)
+  if index.between?(0,8) && !place_taken?(board, index)
+  true
+  elsif
+  false
+  
+ end
+end
+
+def turn(board)
+  puts "Please enter 1–9:"
+  input = gets.strip
+  index = input_to_index
+ if valid_move?(broad, index)
+   move(board, index, current_player)
+   display_board(board)
+   elsif
+   turn(board)
+ end
+  
+end
